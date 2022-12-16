6bc1b075bbe0fd8db5f80301096c2210d5ff1fe7
Amir Drljevic <amirdrljevic@gmail.com>
2022-12-16 21:50:31 +0100

    Line item dates added

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :quotes
+  #resources :quotes
 
   root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :quotes do
+    resources :line_item_dates, except: [:index, :show]
+  end
 end
